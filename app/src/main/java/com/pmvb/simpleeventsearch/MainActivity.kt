de51d3179dc7a8383674befb01f9cbc32ead861b
Martín Vásquez <pedromvasquezb@gmail.com>
2020-02-13 21:54:54 -0500

    Initial commit

---
+++ app/src/main/java/com/pmvb/simpleeventsearch/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.pmvb.simpleeventsearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pmvb.simpleeventsearch.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/pmvb/simpleeventsearch/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.pmvb.simpleeventsearch.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
